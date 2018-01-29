92b55b131aa3c4b334e4fb61b3038fc703138245
loreliji <loreliji@gmail.com>
2018-01-28 23:14:32 -0500

    Creación del archivo listar de las tablas cita y  estado_cita

---
+++ estado_cita.php
@@ -0,0 +1,13 @@
+<?php
+    require_once "conexion.php";
+
+    $con = new conector_pg();
+    $sql1 = 'SELECT * FROM estado_cita';
+    $result=$con->consultar($sql1);
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                foreach ($line as $col_value) {
+                    echo $col_value;
+                }
+            }
+    $con->__destruct();
+?>
