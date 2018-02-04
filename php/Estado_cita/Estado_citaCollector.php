1b4014b568fa9ae33a5fa344f2b8d5f4e30638b6
loreliji <loreliji@gmail.com>
2018-02-04 18:29:24 -0500

    Creación de archivo PHP Collector Estado_Cita

---
+++ php/Estado_cita/Estado_citaCollector.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once('Estado_cita.php');
+include_once('Collector.php');
+
+class Estado_CitaCollector extends Collector
+{
+  
+  function showEstado_citas() {
+    $rows = self::$db->getRows("SELECT * FROM estado_cita ");        
+    
+    $arrayEstado_cita = array();        
+    foreach ($rows as $c){
+      $aux = new Estado_cita($c{'idestadocita'},$c{'descripcion'});
+      array_push($arrayEstado_cita, $aux);
+    }
+    return $arrayEstado_cita;        
+  }
+    
+  function showEstado_cita($id){
+    $row = self::$db->getRows("SELECT * FROM estado_cita where idestadocita= ? ", array("{$id}"));
+
+    $ObjRol = new Rol($row[0]{'idestadocita'},$row[0]{'descripcion'});
+    return $ObjRol;
+
+}
+
+
+function updateEstado_cita($id,$descripcion){
+	$insertrow = self::$db->updateRow("UPDATE public.estado_cita SET descripcion= ? WHERE idestadocita= ?", array("{$descripcion}", $id));
+
+}
+
+function deleteEstado_cita($id){
+	$deleterow = self::$db->deleteRow("DELETE FROM public.estado_cita WHERE idestadocita= ?", array("{$id}"));
+
+}
+
+function createEstado_cita($descripcion){
+	$insertarrow = self::$db->insertRow("INSERT INTO public.estado_cita (descripcion) VALUES (?)", array ("{$descripcion}"));
+
+}
+
+}
+?>
+
