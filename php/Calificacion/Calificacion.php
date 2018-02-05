86d0837b93c93f0b1098845b369d4f3f4a2b39e8
loreliji <loreliji@gmail.com>
2018-02-04 19:08:26 -0500

    Modificación en objeto Cita y Calificación

--- php/Calificacion/Calificacion.php
+++ php/Calificacion/Calificacion.php
@@ -36,15 +36,4 @@ class Calificacion
      }
 }
     
-/*    require_once "conexion.php";
-    
-    $con = new conector_pg();
-    $sql1 = 'SELECT * FROM calificacion';
-    $result=$con->consultar($sql1);
-    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-                foreach ($line as $col_value) {
-                    echo $col_value;
-                }
-            }
-    $con->__destruct();*/
 ?>
