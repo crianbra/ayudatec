a201910c7f95a120082f1cd70361ac6fd5317c2d
loreliji <loreliji@gmail.com>
2018-02-13 11:04:13 -0500

    header y aside

---
+++ backend/vistas/aside.php
@@ -0,0 +1,46 @@
+<!--aside start-->
+<aside>
+    <div id="sidebar" class="nav-collapse">
+        <!-- sidebar menu start-->            <div class="leftside-navigation">
+            <ul class="sidebar-menu" id="nav-accordion">
+            <li class="sub-menu">
+                <a href="../personas/index.php">
+                    <i class="fa fa-user"></i>
+                    <span>Personas</span>
+                </a>
+            </li>
+            <li>
+                <a href="../tecnicos/index.php">
+                    <i class="fa fa-group"></i>
+                    <span>Técnicos</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="index.php" class="active">
+                    <i class="fa fa-book"></i>
+                    <span>Citas agendadas</span>
+                </a>
+            </li>
+            <li>
+                <a href="../calificacion/index.php">
+                    <i class="fa fa-comments-o"></i>
+                    <span>Feedback </span>
+                </a>
+            </li>
+            <li class="sub-menu">
+                 <a href="">
+                    <i class="fa fa-cog"></i>
+                    <span>Configuración </span>
+                </a>
+                    <ul class="sub">
+                        <li><a href="../usuarios/index.php">Usuarios</a></li>
+                        <li><a href="../roles/index.php">Roles</a></li>
+                        <li><a href="../estado_citas/index.php">Estado de citas</a></li>
+                        <li><a href="../tecnico_x_categoria/index.php">Categoria de técnicos</a></li>
+                    </ul>
+                </li>
+        </ul></div>        
+<!-- sidebar menu end-->
+    </div>
+</aside>
+<!--aside end-->
