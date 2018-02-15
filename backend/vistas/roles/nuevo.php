0a75b6d6f119633097775c20f73eadf9ff9bf5ec
crianbra <crianbra@espol.edu.ec>
2018-02-14 23:11:55 -0500

    Class Detalle_Calificacion

--- backend/vistas/roles/nuevo.php
+++ backend/vistas/roles/nuevo.php
@@ -42,7 +42,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <link rel="shortcut icon" href="../images/favicon.png">
+    <link rel="shortcut icon" href="../../assets/images/favicon.png">
 
     <title>Nuevo Rol</title>
 
