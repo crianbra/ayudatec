e36fb535a57f9ceb7d9bd58174571b9e7c0ea957
crianbra <crianbra@espol.edu.ec>
2018-02-15 16:20:55 -0500

    cambio

--- backend/vistas/personas/editar.php
+++ backend/vistas/personas/editar.php
@@ -3,7 +3,7 @@
     include_once("../constantes.php");
     $guardado = false;
     $msg = "";
-    if (isset($_GET['id']) && $_GET['nombre'] ==! "") {
+    if (isset($_GET['id']) && $_GET['id'] ==! "") {
         session_start();
         include_once("../../collectors/personaCollector.php");
         $PersonaCollectorObj = new PersonaCollector();
