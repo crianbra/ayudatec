af01c14ef79240161807cd395692df7abc303f6b
crianbra <crianbra@espol.edu.ec>
2018-02-15 17:36:17 -0500

    Prueba

---
+++ backend/vistas/roles/guardar.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+</head>
+<body>
+
+<div id="main">
+
+<?php
+$descripcion = $_POST["descripcion"];
+
+//echo 'Hola ' .htmlspecialchars($descripcion) . '!';
+include_once("../../rolCollector.php");
+
+$RolCollectorObj = new RolCollector();
+$ObjRol = $RolCollectorObj->createRol($descripcion);
+
+echo "Se ha guardado correctamente </br>";
+
+?>
+
+<div><a href="index.php">Volver al Inicio</a></div>
+</div>
+</body>
+</html>
---
+++ backend/vistas/roles/insertar.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+
+?>
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>Insertar</title>
+</head>
+<body>
+
+<h1>Insertar Rol</h1>
+
+
+<form action="guardar.php" method="post">
+            <fieldset>
+		<label>Descripcion</label>
+		<input type="text" name="descripcion" autofocus required />
+        <br />
+                <button type="submit">Guardar</button>
+        </fieldset>
+	</form>
+
+ 
+
+</body>
+</html>
