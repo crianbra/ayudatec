8fa16f965e59c95258d7a9f8cf2dc7de91c3b59a
crianbra <crianbra@espol.edu.ec>
2018-02-15 19:36:56 -0500

    Prueba

--- backend/vistas/roles/insertar.php
+++ backend/vistas/roles/insertar.php
@@ -15,15 +15,13 @@ include_once('../../models/rol.php');
 
 
 <form action="guardar.php" method="post">
-    <fieldset>
-        
-        <div class="form-group">
+            <div class="form-group">
             <label for="descricpion">Descripcion</label>
             <input type="text" class="form-control" id="descripcion" name="descripcion" placeholder="Descripcion" rows="6" autofocus required>
         </div>
         
         <button type="submit">Guardar</button>        
-    </fieldset>
+    
 </form>
 
  
