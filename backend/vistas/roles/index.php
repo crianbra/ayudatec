852130f594d51f7b5c831595201565f92b73be3a
crianbra <crianbra@espol.edu.ec>
2018-02-15 20:53:38 -0500

    Prueba

--- backend/vistas/roles/index.php
+++ backend/vistas/roles/index.php
@@ -7,7 +7,7 @@ session_start();
 
 
     if (!isset($_SESSION['user'])){
-            echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../../../registro.php'>";
+            echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../admin.php'>";
         }else{
             if(!$_SESSION['rol']==1){
                 echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../index.php'>";
